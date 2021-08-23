071a22a2d8c0030d2b85646dd48431c43ee86f86
noriaki_tanimichi <tanimichi@morality-line.com>
2021-08-24 00:06:08 +0900

    引数付きのAction

---
+++ Assets/Script/LambdaTest0103.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LambdaTest0103 : MonoBehaviour
+{
+	private System.Action<string> eventAction;
+	void Start()
+	{
+		eventAction = Test;
+		eventAction("引数");
+	}
+
+	private void Test(string argText)
+	{
+		Debug.Log(argText + "付きでTestが実行されたよ");
+	}
+}
