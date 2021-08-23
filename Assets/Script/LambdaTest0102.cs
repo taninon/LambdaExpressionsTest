3a509ccfa91a2f89a8f1c084a05b992aef04603a
noriaki_tanimichi <tanimichi@morality-line.com>
2021-08-23 23:55:59 +0900

    テストコード追加

---
+++ Assets/Script/LambdaTest0102.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LambdaTest0102 : MonoBehaviour
+{
+	private System.Action eventAction;
+	private void Start()
+	{
+		eventAction += Test1;
+		eventAction += Test2;
+		eventAction();
+	}
+
+	private void Test1()
+	{
+		Debug.Log("Test1が実行されたよ");
+	}
+	private void Test2()
+	{
+		Debug.Log("Test2が実行されたよ");
+	}
+}
