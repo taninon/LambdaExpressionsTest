b56f6f47f8374aebd2aade7a7262dd630b8416a4
noriaki_tanimichi <tanimichi@morality-line.com>
2021-08-24 01:35:00 +0900

    HitEvent

---
+++ Assets/Script/HitController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitController : MonoBehaviour
+{
+	public Action hitEvent;
+
+	void Start()
+	{
+		hitEvent += Hit;
+	}
+
+	private void Hit()
+	{
+		Debug.Log("何かしら当たった");
+	}
+
+}
---
+++ Assets/Script/HitEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEvent : MonoBehaviour
+{
+	[SerializeField] HitController hitController;
+
+	void Start()
+	{
+		hitController.hitEvent += ChengeColor;
+	}
+	private void ChengeColor()
+	{
+		this.gameObject.GetComponent<Renderer>().material.color = Color.blue; ;
+	}
+	private void OnCollisionEnter(Collision collision)
+	{
+		hitController.hitEvent();
+	}
+}
